8ff00be84594e1ffb8568ff142c31a8827a55f72
Fabrizio Codello <fabryz@gmail.com>
2013-04-21 19:02:17 +0200

    Created EvankoQuinnUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
 
             new EvankoQuinn\SiteBundle\EvankoQuinnSiteBundle(),
+            new EvankoQuinn\UserBundle\EvankoQuinnUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/EvankoQuinn/UserBundle/EvankoQuinnUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace EvankoQuinn\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EvankoQuinnUserBundle extends Bundle
+{
+}
