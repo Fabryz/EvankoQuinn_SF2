9fcb354e6faf3dab34f18ecb91659e3bb7e356af
Fabrizio Codello <fabryz@gmail.com>
2013-04-21 21:39:31 +0200

    Added Stof\DoctrineExtensionsBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
 
             new FOS\UserBundle\FOSUserBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
 
             new EvankoQuinn\SiteBundle\EvankoQuinnSiteBundle(),
             new EvankoQuinn\UserBundle\EvankoQuinnUserBundle(),
